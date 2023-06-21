858b969cbb6b12bd868a2cee4c851d950d0002a9
aminnaeem <amin.naeem1233@gmail.com>
2023-06-21 23:06:18 +0500

    SMIT Dart Assignment#2 Class 4

---
+++ 2-Q5.dart
@@ -0,0 +1,10 @@
+void main(){
+    List Numbers = [20,40,61,89,32,27,53,48];
+    Numbers.sort();
+    var Smallest = Numbers.first;
+    var Greatest = Numbers.last;
+
+    print('Smallest number of the list is $Smallest');
+    print('Greatest number of the list is $Greatest');
+
+}

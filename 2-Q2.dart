fac9c0eba2118cb6d588ec37dcebc21f61ae899b
aminnaeem <amin.naeem1233@gmail.com>
2023-06-22 19:45:06 +0500

    Updated Q2 code of SMIT Dart Assignment#2 Class

--- 2-Q2.dart
+++ 2-Q2.dart
@@ -1,5 +1,6 @@
 void main() {
   List Days = [];
-  Days.addAll(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']);
+  Days.addAll(['Monday','Tuesday','Wednesday','Thursday','Friday']);
+  Days.insertAll(Days.length, ['Saturday', 'Sunday']);
   print(Days);
 }

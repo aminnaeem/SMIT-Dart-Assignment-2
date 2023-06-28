78d09b6743da453e624bb0c7fdf966485d31100b
aminnaeem <amin.naeem1233@gmail.com>
2023-06-28 23:54:02 +0500

    SMIT Dart Assignment#2 Class 4

--- 2-Q4.dart
+++ 2-Q4.dart
@@ -1,61 +1,8 @@
 void main() {
-  List Numbers = [1,2,3,4,5,6,7,8,9];
-  List Type = [];
-  if(Numbers[0]%2 == 0){
-    Type.insert(0, 'True');
-  }
-  else{
-    Type.insert(0, 'False');
-  }
-  if(Numbers[1]%2 == 0){
-    Type.insert(1, 'True');
-  }
-  else{
-    Type.insert(1, 'False');
-  }
-  if(Numbers[2]%2 == 0){
-    Type.insert(2, 'True');
-  }
-  else{
-    Type.insert(2, 'False');
-  }
-  if(Numbers[3]%2 == 0){
-    Type.insert(3, 'True');
-  }
-  else{
-    Type.insert(3, 'False');
-  }
-  if(Numbers[4]%2 == 0){
-    Type.insert(4, 'True');
-  }
-  else{
-    Type.insert(4, 'False');
-  }
-  if(Numbers[5]%2 == 0){
-    Type.insert(5, 'True');
-  }
-  else{
-    Type.insert(5, 'False');
-  }
-  if(Numbers[6]%2 == 0){
-    Type.insert(6, 'True');
-  }
-  else{
-    Type.insert(6, 'False');
-  }
-  if(Numbers[7]%2 == 0){
-    Type.insert(7, 'True');
-  }
-  else{
-    Type.insert(7, 'False');
-  }
-  if(Numbers[8]%2 == 0){
-    Type.insert(8, 'True');
-  }
-  else{
-    Type.insert(8, 'False');
-  }
-  print(Numbers);
-  print(Type);
+  Map<String , String> Clients =  {
+    'name':'Ahmed',
+    'phone':'0301-1234567'
+  };
 
+  print(Clients.keys.where((e) => e.length == 4 ));
 }

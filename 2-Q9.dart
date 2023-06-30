f4f68abfe84b1b600fa3905304541b39b23a9995
aminnaeem <amin.naeem1233@gmail.com>
2023-06-30 22:50:48 +0500

    SMIT Dart Assignment#2 Class 4

---
+++ 2-Q9.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  List<int> Integer = [15 , 8 , 12 , 25 , 24 , 32 , 28 , 17];
+    Integer.sort();
+    int Min = Integer.first;
+    int Max = Integer.last;
+
+
+    print('Maximum Value : $Max');
+}

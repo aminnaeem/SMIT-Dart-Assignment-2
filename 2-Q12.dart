8adf0f76c45298cd20c6b225d7e031a54d091ba6
aminnaeem <amin.naeem1233@gmail.com>
2023-07-03 22:07:31 +0500

    SMIT Dart Assignment#2 Class 4

---
+++ 2-Q12.dart
@@ -0,0 +1,9 @@
+void main() {
+
+  List<String> Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+  List<String> MonthsReversed = List.from(Months.reversed);
+
+  print(Months);
+  print(MonthsReversed);
+
+}
